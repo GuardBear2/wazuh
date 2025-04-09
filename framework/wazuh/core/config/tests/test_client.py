e9b69ce1409226631d9b176e68f1c9c65f48f51e
GGP1 <gaston.palomeque@wazuh.com>
2025-04-09 12:29:51 -0300

    Update server configuration

--- framework/wazuh/core/config/tests/test_client.py
+++ framework/wazuh/core/config/tests/test_client.py
@@ -10,7 +10,7 @@ from wazuh.core.config.models.central_config import (
     IndexerConfig,
     ManagementAPIConfig,
 )
-from wazuh.core.config.models.server import DEFAULT_SERVER_INTERNAL_CONFIG, ServerConfig, ValidateFilePathMixin
+from wazuh.core.config.models.server import ServerConfig, ValidateFilePathMixin
 
 mock_config_data = {
     'server': {
@@ -75,12 +75,6 @@ def test_get_server_config(patch_load):
     """Check the correct behavior of the `get_server_config` class method."""
     server_config = CentralizedConfig.get_server_config()
     assert server_config == ServerConfig(**mock_config_data['server'])
-
-
-def test_get_server_internal_config(patch_load):
-    """Check the correct behavior of the `get_internal_server_config` class method."""
-    internal_config = CentralizedConfig.get_internal_server_config()
-    assert internal_config == DEFAULT_SERVER_INTERNAL_CONFIG
 
 
 @pytest.mark.parametrize(
